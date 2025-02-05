5c7253066470e29d0f9d1bb25938e302d400685b
nagmat1 <nagmat@gmail.com>
2025-02-04 22:24:49 -0800

    Second Commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+First Right click on the Project : 
+```
+Run As --> 7 Maven Install 
+```
+
+
 How to run Spring Boot API from terminal : 
 
 ```
@@ -5,3 +11,11 @@ nagmatnazarov@VP27VD291P productrestapi % java -jar target/productrestapi-0.0.1-
 ```
 
 ![Screenshot 2025-02-03 at 3 53 00 PM](https://github.com/user-attachments/assets/d0e8e0c6-8d40-4299-b21b-18669144954d)
+
+Command + SHIFT + T : Open Type (Filename)
+
+For Debug mode : 
+
+```
+java -jar target/productrestapi-0.0.1-SNAPSHOT.jar --debug
+```
--- src/main/java/com/nagmat/springweb/controllers/ProductRestController.java
+++ src/main/java/com/nagmat/springweb/controllers/ProductRestController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,24 @@ public class ProductRestController {
 	
 	@RequestMapping(value="/products/{id}",method=RequestMethod.GET)
 	public Product getProduct(@PathVariable("id") int id){
+		System.out.println("Product id=== "+repository.findById(id).get().getId());
 		return repository.findById(id).get();
+	}
+	
+	@RequestMapping(value="/products/",method=RequestMethod.POST)
+	public Product createProduct(@RequestBody Product product) {
+		return repository.save(product);
+	}
+	
+	@PutMapping("/products/{id}")
+	public Product updateProduct(@RequestBody Product product) {
+		System.out.println("Product = "+product.getDescription());
+		return repository.save(product);
+	}
+	
+	@RequestMapping(value="/products/{id}",method=RequestMethod.DELETE)
+	public void delteProduct(@PathVariable("id") int id) {
+		repository.deleteById(id);
 	}
 	
 }
